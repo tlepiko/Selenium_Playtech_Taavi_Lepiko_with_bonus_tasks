2343c4c93b3cc2aebc00cb3ab525e67c3c7245bd
tlepiko <60349843+tlepiko@users.noreply.github.com>
2023-03-25 21:13:01 +0200

    Task complete with bonus tasks
    Complete main task with bonus tasks:
    -Uses coordinates to click on the Internship tab
    -Writes test result into a text file
    -Converted into Junit framework
    Test actions:
    -Opens Chrome
    -Opens Internship tab
    -Searches for "Development QA Engineer (Intern)" position
    -Confirms wether position is posted or not
    -Closes browser

--- src/Playtech_Internship_check_Main/CheckInternship.java
+++ src/Playtech_Internship_check_Main/CheckInternship.java
@@ -1,48 +1,62 @@
 package Playtech_Internship_check_Main;
 
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
-
-public class CheckInternship {
-
-	public static void main(String[] args) {
+public class CheckInternship {		
+		WebDriver driver;
+		@Before
+		public void initialization() {
+			//Get Chrome driver location.
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			System.setProperty("webdriver.chrome.driver","Chrome_Driver\\chromedriver.exe");
+			driver = new ChromeDriver(options);
+			driver.manage().window().maximize();
+			driver.get("https://www.playtech.ee");
 		
-		//Get Chrome driver location.
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Taavi\\Downloads\\Selenium project files\\chromedriver_win32\\chromedriver.exe");
-		
-		
-		//Set Chrome options to enable control by 3rd party applications.
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		
-		//Launch Chrome in a maximized window.
-		WebDriver driver = new ChromeDriver(options);
-		driver.manage().window().maximize();
-
-		//URL to direct Chrome to.
-		driver.get("https://www.playtech.ee");
-		
-		//Find the "Internship" tab on the playtech.ee website and click it.
-		WebElement internship = driver.findElement(By.xpath("//a[contains(text(), 'Internship')]"));
-		internship.isDisplayed();
-		internship.isEnabled();
-		internship.click();
-		
-		//Search for "Development QA Engineer (Intern)" position on the Internship page. If found confirm existence, else deny existence.
-		boolean development = driver.getPageSource().contains("Development QA Engineer (Intern)");
-		if (development) {
-			System.out.println("The position is shown on the page!");
-		} else {
-			System.out.println("The position is not shown on the page!");
+			//Find the "Internship" tab on the playtech.ee website and click it.
+			WebElement internship = driver.findElement(By.xpath("//a[contains(text(), 'Internship')]"));
+			int x = internship.getLocation().x;
+			int y = internship.getLocation().y;
+			int width = internship.getSize().getWidth();
+		    Actions act = new Actions(driver);
+		    act.moveByOffset(x+width/2, y).click().perform();		
 		}
 		
-		//Close Chrome.
-		driver.quit();
+		@Test
+		public void checkInternship() {
+			//Search for "Development QA Engineer (Intern)" position on the Internship page. If found confirm existence, else deny existence.
+			boolean development = driver.getPageSource().contains("Development QA Engineer (Intern)");
+			try {
+				if (development) {
+					FileOutputStream result = new FileOutputStream("results\\result.txt");
+					System.setOut(new PrintStream(result));
+					System.out.println("The position is shown on the page!");
+				} else {
+					FileOutputStream result = new FileOutputStream("results\\result.txt");
+					System.setOut(new PrintStream(result));
+					System.out.println("The position is not shown on the page!");
+				}
+			} catch(FileNotFoundException e) {
+				System.out.println(e);
+			}
+		}
 
+		//Close browser.
+		@After
+		public void close() {
+			driver.quit();
+		}
 	}
-
-}
